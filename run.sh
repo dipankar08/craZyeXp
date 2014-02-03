41ad200363ccdbbaec7b783e620eb1fa3f11e609
Dipankar Dutta <dutta.dipankar08@gmail.com>
2014-02-03 18:51:49 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ git stash
 git pull
 git stash apply
 git add *
-git rm --cached ChatEngine/config.ini
+git rm --cached config.ini
 git commit -m "Automated Chek In to craZyeXp"
 git push -u origin master
