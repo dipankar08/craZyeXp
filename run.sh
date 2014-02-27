e5cac495cc9f4200d355aadd55303a999e7be3fc
Dipankar <dipankar@dipankar-OptiPlex-390.(none)>
2014-02-27 11:30:57 +0530

    Automated Chek In to craZyeXp

--- run.sh
+++ run.sh
@@ -2,9 +2,14 @@
 find . -name "*.pyc" -exec git rm -f {} \;
 #ignore but keep local all ini file.
 find . -name "*.ini" -exec git rm --cached {} \;
+
 git stash
 git pull
 git stash apply
+
+# Actually Remove these stuff
+find . -name "*.pyc" -exec rm -rf {} \;
+
 #git add *
 #git rm --cached config.ini
 git commit -m "Automated Chek In to craZyeXp"
