cc6274c9464282ee2e392769efed237477b2138c
Dipankar <dipankar@dipankar-OptiPlex-390.(none)>
2014-02-27 10:59:32 +0530

    Done

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
+#ignore all pyc file
+find . -name "*.pyc" -exec git rm -f {} \;
+#ignore but keep local all ini file.
+find . -name "*.ini" -exec git rm --cached {} \;
 git stash
 git pull
 git stash apply
 #git add *
-git rm --cached config.ini
+#git rm --cached config.ini
 git commit -m "Automated Chek In to craZyeXp"
 git push -u origin master
