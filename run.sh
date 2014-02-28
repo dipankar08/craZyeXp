4f92625ff4433d43c6cfd208ed7eb7725035b90a
unknown <Dipankar.Dutta@BLRLT1094.ipinfusion.com>
2014-02-28 10:38:47 +0530

    hello

--- run.sh
+++ run.sh
@@ -1,16 +1,25 @@
 #ignore all pyc file
-find . -name "*.pyc" -exec git rm -f {} \;
+#find . -name "*.pyc" -exec git rm -f {} \;
 #ignore but keep local all ini file.
-find . -name "*.ini" -exec git rm --cached {} \;
+#find . -name "*.ini" -exec git rm --cached {} \;
 
 git stash
 git pull
 git stash apply
 
 # Actually Remove these stuff
-find . -name "*.pyc" -exec rm -rf {} \;
+#find . -name "*.pyc" -exec rm -rf {} \;
 
-#git add *
-#git rm --cached config.ini
+# Add everything.
+git add *
+
+# remove Unnecessary -- ADD Temp Dict Here..
+find . -name "*.pyc" -exec git rm -f {} \;
+find . -name "*.tmp" -exec git rm -f {} \;
+find . -name "*.log" -exec git rm -f {} \;
+find . -name "*.ini" -exec git rm --cached {} \;
+git rm --cached config.ini
+
+
 git commit -m "Automated Chek In to craZyeXp"
 git push -u origin master
