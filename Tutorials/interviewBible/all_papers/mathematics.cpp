7a5af10552602ed535ec1adc7d903486cd1803e2
Dipankar <dipankar@dipankar-OptiPlex-390.(none)>
2014-06-09 14:53:43 +0530

    R

--- Tutorials/interviewBible/all_papers/mathematics.cpp
+++ Tutorials/interviewBible/all_papers/mathematics.cpp
@@ -63,17 +63,38 @@ Sol 2:  Some efiicinet techn is dr, which solve O(log x +log y) i.e. Sum of bits
 
 
 /*******************************************************************************
-* Problem: 
-* Input:
-* Output:
+* Problem: Find the smallest number whose digits multiply to a given number n 
+* Input / Output:
+Input:  n = 36
+Output: p = 49 
+// Note that 4*9 = 36 and 49 is the smallest such number
 * Algorithms:
+Case 1: n < 10 When n is smaller than n, the output is always n+10. 
+For example for n = 7, output is 17. For n = 9, output is 19.
+
+Case 2: n >= 10 Find all factors of n which are between 2 and 9 (both inclusive).
+The idea is to start searching from 9 so that the number of digits in result are minimized.
+For example 9 is preferred over 3*3 and 8 is preferred over 2*4.
+Store all found factors in an array. 
+The array would contain digits in non-increasing order, so finally print the array in reverse order.
 *******************************************************************************/
 
 
 /*******************************************************************************
-* Problem: 
-* Input:
-* Output:
+* Problem: Write a function that generates one of 3 numbers according to given probabilities 
+* Input / Output:
+You are given a function rand(a, b) which generates equiprobable random numbers between [a, b] inclusive.
+Generate 3 numbers x, y, z with probability P(x), P(y), P(z) such that P(x) + P(y) + P(z) = 1 using the given rand(a,b) function.
+
+The idea is to utilize the equiprobable feature of the rand(a,b) provided.
+Let the given probabilities be in percentage form, for example P(x)=40%, P(y)=25%, P(z)=35%..
+
+Following are the detailed steps.
+1) Generate a random number (r) between 1 and 100. Since they are equiprobable, the probability of each number appearing is 1/100.
+2) Following are some important points to note about generated random number r.
+a) r is smaller than or equal to P(x) with probability P(x)/100.
+b) r is greater than P(x) and smaller than or equal P(x) + P(y) with P(y)/100.
+c) r is greater than P(x) + P(y) and smaller than or equal 100 (or P(x) + P(y) + P(z)) with probability P(z)/100
 * Algorithms:
 *******************************************************************************/
 
