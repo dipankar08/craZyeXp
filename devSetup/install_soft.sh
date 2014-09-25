810e5157e3f018401b5bdd1e931206d22e7c0ffe
Your Name <you@example.com>
2014-09-25 21:20:11 +0530

    ins

--- devSetup/install_soft.sh
+++ devSetup/install_soft.sh
@@ -1,14 +1,14 @@
 # This is start script to install necessary package for my development
-sudo su -
-apt-get install openssh-server
+#sudo su -
+sudo apt-get install openssh-server
 
 # Install Basic Package
-apt-get install git vim g++ screen valgrind ctags cscope 
+suod apt-get install git vim g++ screen valgrind ctags cscope 
 
 # Python related Installation.
-wget -P Downloads/ http://python-distribute.org/distribute_setup.py && python Downloads/distribute_setup.py && sudo easy_install pip && which pip
-
-pip install quicksms
+#sudo wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py && which pip
+sudo apt-get install python-pip
+sudo pip install quicksms
 
 # Bug Fix
 echo 'gtk-key-theme-name = "Emacs"' > ~/.gtkrc-2.0 # backspace
