b4d7b219cd1e38a2962e8d10d33301b55ac9046c
Your Name <you@example.com>
2014-10-16 18:28:36 +0530

    ajs

---
+++ MiniProjects/schoolDocs/software.sh
@@ -0,0 +1,7 @@
+sudo apt-get install npm
+sudo npm install -g grunt-cli
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo apt-get install ruby
+sudo apt-get install gem
+sudo gem install sass
+sudo apt-get install default-jre
