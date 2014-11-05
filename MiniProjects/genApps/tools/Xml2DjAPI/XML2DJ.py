00de2b272c78ebbe42f5418aff1bf729f8ef162c
Your Name <you@example.com>
2014-11-05 19:22:57 +0530

    Auto Checkin on Wed Nov  5 19:22:57 IST 2014

--- MiniProjects/genApps/tools/Xml2DjAPI/XML2DJ.py
+++ MiniProjects/genApps/tools/Xml2DjAPI/XML2DJ.py
@@ -44,13 +44,21 @@ cc = codegen.CodeGenerator() # common.py
 
 cc *="""
 import sys, traceback
+import os
+
+   
 def D_LOG():
+  import pdb  
+  print '_'*60  
+  _, _, tb = sys.exc_info()
+  filename, lineno, funname, line = traceback.extract_tb(tb)[-1]
+  print "Exception in user code:",filename,':',lineno,')'
   print '-'*60
-  print "Exception in user code:"
+  os.system('sed -n '+str(lineno-5)+','+str(lineno+5)+'p '+filename)
+  print '-'*60
   traceback.print_exc(file=sys.stdout)
-  print '-'*60
-  import pdb
-  pdb.set_trace()
+  print '_'*60
+  #pdb.set_trace()
 """
 
 ms += """
