eee3ef691cacbe94ce0cef054186211c42914a50
Your Name <you@example.com>
2014-11-18 03:41:57 +0530

    Auto Checkin on Tue Nov 18 03:41:57 IST 2014

--- MiniProjects/QuickEdit/script.js
+++ MiniProjects/QuickEdit/script.js
@@ -11,10 +11,31 @@ $(document).ready(function() {
 $('#html').bind('input propertychange', function() {
 console.log(this.value);
 var html = this.value;
+/* Will Do some Majic here */
+
 var content = $("#preview").contents().find("body"); // iframe id is 'preview'
 content.html(html);
      
 });
+
+$('#html').keypress(function(event){
+       
+	var keycode = (event.keyCode ? event.keyCode : event.which);
+   { /* auto ending closing tag */    
+        a = this.value.slice(0,this.selectionStart)
+        b = this.value
+        //alert('You pressed a "enter" key in textbox'+this.value+"#"+keycode+"#"+this.selectionStart+"#"+this.selectionEnd);	
+        if(keycode == 62 && a[a.lastIndexOf('<')+1]!='/')
+       {   
+           tag = a.substring(a.lastIndexOf('<')+1,this.selectionStart)
+           inj = "></"+tag+">"
+           this.value = b.substring(0,this.selectionStart)+inj+b.substring(this.selectionStart,b.length)
+           this.value = this.value.slice(0,this.value.length -1);
+
+       } 
+    }
+     
+ });
 
 $('#css').bind('input propertychange', function() {
 
@@ -50,3 +71,5 @@ console.error = console.debug = console.info =  console.log
 
 
 });
+/*********  end of code ***************/
+
