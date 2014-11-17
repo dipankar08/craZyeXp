62350905ae3da5f9a29064303475c92917e1bd4b
Your Name <you@example.com>
2014-11-18 01:56:36 +0530

    Auto Checkin on Tue Nov 18 01:56:36 IST 2014

--- MiniProjects/QuickEdit/script.js
+++ MiniProjects/QuickEdit/script.js
@@ -1,35 +1,12 @@
     angular.module('dipankar', ['ngSanitize'])
       .controller('ExampleController', ['$scope', '$sce', function($scope, $sce) {
-        $scope.html =
-          '<p style="color:blue">an html\n' +
-          '<em onmouseover="this.textContent=\'PWN3D!\'">click here</em>\n' +
-          'snippet</p>';
+        $scope.html ='';
         $scope.deliberatelyTrustDangerousSnippet = function() {
           return $sce.trustAsHtml($scope.html);
         };
       }]);
 
 $(document).ready(function() {
-
-
-    var html = "<div>Hello from iframe</div>"; // HTML code
-    var content = $("#preview").contents().find("body"); // iframe id is 'preview'
-
-    content.html(html);
-
-    var csVal = "div { color: red; font-size: 40px;}";
-    var cssLink = "<style>" + csVal + "</style>"; // cssVal contains css code
-    var head = $("#preview").contents().find("head");
-    head.append(cssLink);
-
-    var jsCode = "alert('you are in the iframe')";
-
-    var js ='<script>'+jsCode+'<\/script>' ; 
-
-    // following part is not working
-    var content = $('#preview').contents();
-    content.find('head').append('<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"><\/script>' );             
-    content.find('body').append(js );
 
 $('#html').bind('input propertychange', function() {
 console.log(this.value);
@@ -67,7 +44,7 @@ if (typeof console  != "undefined")
 
 console.log = function(message) {
     console.olog(message);
-    $('#debugDiv').append('<p>' + message + '</p>');
+    $('#debugDiv').html(message);
 };
 console.error = console.debug = console.info =  console.log
 
