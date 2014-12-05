ee3f88e2474a6974d66318680bd0ad241174fb9a
Your Name <you@example.com>
2014-12-05 18:46:59 +0530

    Auto Checkin on Fri Dec  5 18:46:59 IST 2014

--- MiniProjects/genApps/static/js/toothstrap.js
+++ MiniProjects/genApps/static/js/toothstrap.js
@@ -67,6 +67,41 @@ function clearPopup_box() {
   }
   
 /**********************Start Overlay-Popup ******************/
+
+
+/*****************  Start full screen ***********************/
+window.toggleFullScreen  = function(elem) {
+    if ((document.fullScreenElement && document.fullScreenElement !== null) || (document.msfullscreenElement && document.msfullscreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+        if (elem.requestFullScreen) {
+            elem.requestFullScreen();
+        } else if (elem.mozRequestFullScreen) {
+            elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullScreen) {
+            elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        } else if (elem.msRequestFullscreen) {
+            elem.msRequestFullscreen();
+        }
+       $( "body" ).addClass( "fullscreen" )
+    } else {
+        if (document.cancelFullScreen) {
+            document.cancelFullScreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+         $( "body" ).removeClass( "fullscreen" )
+    }
+}
+/************** End of fll screen ******************************/
+
+
  
 })
+
+
+
+/*full screen */
 
