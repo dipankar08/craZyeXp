8cd935a26da94c5006e9480e73a04eabe1d6c5dc
Your Name <you@example.com>
2015-01-30 15:52:15 +0530

    Auto Checkin on Fri Jan 30 15:52:15 IST 2015

--- MiniProjects/genApps/static/js/toothstrap.js
+++ MiniProjects/genApps/static/js/toothstrap.js
@@ -431,3 +431,101 @@ $("#test").html(html)
 
 });
 /******************  End of Tree Menu Js *****************/
+
+/************************** Auto Hide Popup ***********************/
+function autohideMsgPopUp(html) {
+  $(".autohideMsgPopUp").html(html);
+  $(".autohideMsgPopUp").addClass('active')
+  setTimeout(function(){  $(".autohideMsgPopUp").removeClass('active'); }, 2000);
+}
+//shoub be Moved to common lib
+function autohide(id,sec=0) {
+  $(id).addClass('active')
+  setTimeout(function(){  $(id).removeClass('active'); }, sec*1000);
+}
+/**********************************************************************/
+
+
+
+/******************  Bubble Games *****************/
+
+function bubbles() {  
+  // Settings
+  var min_bubble_count = 20, // Minimum number of bubbles
+      max_bubble_count = 60, // Maximum number of bubbles
+      min_bubble_size = 3, // Smallest possible bubble diameter (px)
+      max_bubble_size = 12; // Maximum bubble blur amount (px)
+  var clr=['red','green','blue','yellow','black']
+  
+  // Calculate a random number of bubbles based on our min/max
+  var bubbleCount =  min_bubble_count + Math.floor(Math.random() * (max_bubble_count + 1));
+  
+  var $bubbles = $('.bubbles'); 
+  // Create the bubbles
+  for (var i = 0; i < bubbleCount; i++) {
+    $bubbles.append('<div class="bubble-container"><div class="bubble"></div></div>');
+  }  
+  // Now randomise the various bubble elements
+  $bubbles.find('.bubble-container').each(function(){    
+    
+    var pos_rand = Math.floor(Math.random() * 101); // Randomise the bubble positions (0 - 100%)   
+    var size_rand = min_bubble_size + Math.floor(Math.random() * (max_bubble_size + 1));   // Randomise their size 
+    var delay_rand = Math.floor(Math.random() * 16);    // Randomise the time they start rising (0-15s)
+    var speed_rand = 3 + Math.floor(Math.random() * 9);   // Randomise their speed (3-8s)    
+    var bdr_clr = clr[Math.floor(Math.random() * clr.length)];   // Randomise their speed (3-8s)   
+    var blur_rand = 0//Math.floor(Math.random() * 3);  // Random blur 
+    
+    
+    // Cache the this selector
+    var $this = $(this);    
+    // Apply the new styles
+    $this.css({
+      'left' : pos_rand + '%',      
+      '-webkit-animation-duration' : speed_rand + 's',
+      '-moz-animation-duration' : speed_rand + 's',
+      '-ms-animation-duration' : speed_rand + 's',
+      'animation-duration' : speed_rand + 's',      
+      '-webkit-animation-delay' : delay_rand + 's',
+      '-moz-animation-delay' : delay_rand + 's',
+      '-ms-animation-delay' : delay_rand + 's',
+      'animation-delay' : delay_rand + 's',      
+      '-webkit-filter' : 'blur(' + blur_rand  + 'px)',
+      '-moz-filter' : 'blur(' + blur_rand  + 'px)',
+      '-ms-filter' : 'blur(' + blur_rand  + 'px)',
+      'filter' : 'blur(' + blur_rand  + 'px)'
+      
+    });    
+    $this.children('.bubble').css({
+      'width' : size_rand + 'px',
+      'height' : size_rand + 'px',
+      'border-color': bdr_clr
+    });
+    
+  });
+}
+// In case users value their laptop battery life
+// Allow them to turn the bubbles off
+$('.bubble-toggle').click(function(){
+  if($bubbles.is(':empty')) {
+    bubbles();
+    $bubbles.show();
+    $(this).text('Bubbles Off');
+  } else {
+    $bubbles.fadeOut(function(){
+      $(this).empty();
+    });
+    $(this).text('Bubbles On');
+  } 
+  
+  return false;
+});
+
+function startBubbleGame(){
+  var $bubbles = $('.bubbles'); 
+  $bubbles.html(''); //clean all
+  $bubbles.addClass('active')
+  bubbles();
+  setTimeout(function(){  $(".bubbles").removeClass('active'); }, 5000); // play for 30 sec.
+}
+
+/******************  Bubble Games End *****************/
