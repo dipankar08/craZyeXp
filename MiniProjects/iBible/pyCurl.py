07d98ddeb0d6215ee20470bce8bade7d2fa390ac
Your Name <you@example.com>
2015-02-20 01:58:03 +0530

    Auto Checkin on Fri Feb 20 01:58:03 IST 2015

--- MiniProjects/iBible/pyCurl.py
+++ MiniProjects/iBible/pyCurl.py
@@ -22,7 +22,7 @@ for i in b:
     qline = i.replace('Q.','').strip()
     name = qline[:qline.find('#')].strip()
     short_desc = qline[qline.find('#')+1:].strip()
-    curlPost('http://10.171.38.43:7777/api/code/',{'name':name,'short_desc':short_desc,'full_desc':'More about the question like sample Q and Ans ..','intro':'Write Basic Intro ex: Baic way to solve the problem or approach and Algorithm of the below code','solution':'Explane the code with comaplexity','topic':topic})
+    curlPost('http://192.168.56.101:7777/api/code/',{'name':name,'short_desc':short_desc,'full_desc':'More about the question like sample Q and Ans ..','intro':'Write Basic Intro ex: Baic way to solve the problem or approach and Algorithm of the below code','solution':'Explane the code with comaplexity','topic':topic})
   else:
    print '>>>>> Error at line:',i
 
