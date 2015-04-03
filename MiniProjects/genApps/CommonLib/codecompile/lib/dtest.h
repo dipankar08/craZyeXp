84ecd1df90bc9c8bc5a5fda6b2602d1f0f78e42f
Your Name <you@example.com>
2015-04-03 16:33:29 +0530

    Auto Checkin on Fri Apr  3 16:33:29 IST 2015

--- MiniProjects/genApps/CommonLib/codecompile/lib/dtest.h
+++ MiniProjects/genApps/CommonLib/codecompile/lib/dtest.h
@@ -1,4 +1,20 @@
-/* dtest is automated testing by dipankar */
+/*
+ dtest is automated testing by dipankar 
+ here We support Automate Test case for Folowing in and following test cases..
+ 
+ void TEST_INT_TO_INT;
+ void TEST_INT_TO_STR
+ void TEST_INT_TO_INTARR
+ 
+ void TEST_STR_TO_INT
+ void TEST_STR_TO_STR
+ void TEST_STR_TO_INTARR
+ 
+ void TEST_INTARR_TO_INT
+ void TEST_INTARR_TO_STR
+ void TEST_INTARR_TO_INTARR
+ */
+
 #include<stdio.h>
 void TEST_INT_TO_INT(int (*f)(int) ){
   printf("\n==========================================");
@@ -70,6 +86,7 @@ void TEST_INTARR_TO_STR(char* (*f)(int*,int) ){
   char out[100];
   scanf("%d",&tc);
     printf("\nTotal test case  : #%d",tc);
+  scanf ("%[^\n]%*c", out); // this for remove new line
   for(int i=0;i<tc;i++){
   printf("\n\nTest Case        : %d",i);
     scanf("%d",&n);
@@ -92,3 +109,90 @@ void TEST_INTARR_TO_STR(char* (*f)(int*,int) ){
   printf("\n=========================================");
 }
 
+/* 
+Example
+4
+Hello7
+A
+Hello1
+A
+Hello2
+A
+Hello3
+A
+*/
+void TEST_STR_TO_STR(char* (*f)(char* ) ){
+  printf("\n==========================================");
+  printf("\n======= W E L C O M E   D T E S T   ======");
+  printf("\n==========================================");
+  int tc=0,pass=0,fail=0;
+  char in[100];
+  char out[100];
+  scanf("%d",&tc);gets(in);// just ignire this line
+    printf("\nTotal test case  : #%d",tc);
+  
+  for(int i=0;i<tc;i++){
+  printf("\n\nTest Case        : %d",i);
+    gets(in);
+    printf("\nExpected Input   : [%s]",in); 
+    gets(out);
+    printf("\nExpected Output  : [%s]",out);
+    char* exp = f(in);
+    printf("\nObserved Output  : [%s]",exp);
+    printf("\nStatus: "); 
+    if(strcmp(exp,out)==0){printf("PASS");pass++;}
+    else{printf("FAIL");fail++;}
+  }
+  printf("\n============  R E S U L T  ==============");
+  printf("\nPass : %5d            FAIL : %5d    .",pass,fail);
+  printf("\n=========================================");
+}
+
+/* 
+Example:
+4
+Hello7
+100
+Hello1
+100
+Hello2
+100
+Hello3
+100
+*/
+int str2int(char* a){
+  int sum =0;
+  while(*a){
+    if(IS_NUMARIC(*a)){
+      sum = sum*10+(*a-'0');
+    }
+    a++;
+  }
+  return sum;
+}
+void TEST_STR_TO_INT(int (*f)(char* ) ){
+  printf("\n==========================================");
+  printf("\n======= W E L C O M E   D T E S T   ======");
+  printf("\n==========================================");
+  int tc=0,pass=0,fail=0;
+  char in[100];
+  char out[100];
+  scanf("%d",&tc);gets(in);// just ignire this line
+    printf("\nTotal test case  : #%d",tc);
+  
+  for(int i=0;i<tc;i++){
+  printf("\n\nTest Case        : %d",i);
+    gets(in);
+    printf("\nExpected Input   : [%s]",in); 
+    gets(out);
+    printf("\nExpected Output  : [%d]",str2int(out));
+    int exp = f(in);
+    printf("\nObserved Output  : [%d]",exp);
+    printf("\nStatus: "); 
+    if(str2int(out) == exp ){printf("PASS");pass++;}
+    else{printf("FAIL");fail++;}
+  }
+  printf("\n============  R E S U L T  ==============");
+  printf("\nPass : %5d            FAIL : %5d    .",pass,fail);
+  printf("\n=========================================");
+}
