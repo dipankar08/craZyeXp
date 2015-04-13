83482aa70d00dce53524b65b1d29893bfa631816
Your Name <you@example.com>
2015-04-14 02:03:43 +0530

    Auto Checkin on Tue Apr 14 02:03:43 IST 2015

--- MiniProjects/genApps/CommonLib/EmailClient/MailEngine.py
+++ MiniProjects/genApps/CommonLib/EmailClient/MailEngine.py
@@ -14,6 +14,7 @@ class MailEngine:
       self.SMTP_PORT = SMTP_PORT
       self.UNAME=UNAME
       self.PASSWD =PASSWD
+      self.DATA_SOURCE= None
    
   def SendMail(self,sender = 'dipankar@gmail.com',recipient="dutta.dipankar08@gmail.com",subject="test mail",body="Sample Body"):
     try:
@@ -23,7 +24,7 @@ class MailEngine:
                  "MIME-Version: 1.0",
                  "Content-Type: text/html"]
       headers = "\r\n".join(headers)    
-      pdb.set_trace()      
+      #pdb.set_trace()      
       session = smtplib.SMTP(self.SMTP_SERVER, self.SMTP_PORT)
       session.ehlo()
       session.starttls()
@@ -34,8 +35,29 @@ class MailEngine:
       print '>>> Email Send Successfully '
     except Exceception ,e:
       print '>>> ERROR: ',e
-  def SetSchedule(self):
-      pass
+  def Schedule(self,sec=5*60):
+    # This is the Scheduing evnet Which calls Send Mail event from using data source
+    import time      # start the scheduler
+    #pdb.set_trace()
+    if self.DATA_SOURCE == None:
+       print '>>> ERORR No data source found.'
+    if self.TEMPLATE_NAME == None:
+       print '>>> ERROR - No Tempalte assigned'
+    while(1):#Opps No schular works implet by sleep. 
+      data = self.DATA_SOURCE.next()
+      body = self.BuildMailTemplate(self.TEMPLATE_NAME,data);
+      self.SendMail(body=body);
+      print '>>>INFO: Let"s Sleep for next Evnet....',sec,'Secons'
+      time.sleep(sec)
+    
+  def AttachTempalte(self,file):
+      try: # Verify file is present..
+        f = open(file);
+        self.TEMPLATE_NAME= file;
+        f.close()
+      except Exceception ,e:
+        print '>>> ERROR: File Doent Exist. ',e 
+    
   def BuildMailTemplate(self,file,data={'name':'Dipankar'}):
       " Build HTML Email template and fillup with data - Return HTML mail"
       try:
@@ -43,12 +65,35 @@ class MailEngine:
         f = open(file);
         template = Template(f.read())
         res = template.render(data)
-        pdb.set_trace()
+        #pdb.set_trace()
         return res;
       except Exceception ,e:
         print '>>> ERROR: ',e      
+
+  def AttachDataSource(self,pklFile):
+      """ Attach a Data Source which is return a a dict, whcih is used to build a message 
+        return an Iterator..
+        Save :
+        a=[{'a':'a'},{'b':'b'}]
+        f = open("data.pkl","wb")
+        pickle.dump(a,f)
+        f.close()
+        
+      """
+      import pickle
+      try:
+        f = open(pklFile,'rb')
+        obj = pickle.load(f)
+        f.close()
+        itr = iter(obj)
+        self.DATA_SOURCE = itr
+      except Exceception ,e:
+        print '>>> ERROR: NOt able to attach data source ',e      
       
 #Sampel Test is Here.
 m = MailEngine()
-x = m.BuildMailTemplate('Template.html',{'q':'My Question','a':'Ans','link':'http://google.com'});
-m.SendMail(body=x)
+m.AttachTempalte('Template.html')
+m.AttachDataSource('data.pkl')
+m.Schedule(sec=60)
+#x = m.BuildMailTemplate('Template.html',{'q':'My Question','a':'Ans','link':'http://google.com'});
+#m.SendMail(body=x)
