daeee2208799f35626203615b878cf6584e81e2b
Your Name <you@example.com>
2015-05-05 19:18:22 +0530

    Auto Checkin on Tue May  5 19:18:22 IST 2015

--- MiniProjects/genApps/CommonLib/EmailClient/MailEngine.py
+++ MiniProjects/genApps/CommonLib/EmailClient/MailEngine.py
@@ -4,6 +4,7 @@
 #  Schedule Mail on Time.
 #########################################
 import re
+import pickle
 # Replce multile line with one line and trim() in both side
 def Normalize(inn):
   if not isinstance(inn, str): return inn
@@ -45,33 +46,58 @@ class MailEngine:
       session.sendmail(sender, recipient, headers + "\r\n\r\n" + body)
       session.quit()
       print '>>> Email Send Successfully '
+      return True
     except Exception ,e:
       print '>>> ERROR:SendMail ',e
-      traceback.print_stack()
-  def Schedule(self,sec=5*60):
+      #traceback.print_stack()
+    return False
+  def Schedule(self,sec=5*60,start_count=None):
     # This is the Scheduing evnet Which calls Send Mail event from using data source
     import time      # start the scheduler
+    if start_count:
+      self.count = start_count
     #pdb.set_trace()
     if self.DATA_SOURCE == None:
        print '>>> ERORR No data source found.'
     if self.TEMPLATE_NAME == None:
        print '>>> ERROR - No Tempalte assigned'
+    print '>>> INFO: Loading state...'
+    if start_count:
+      self.count = start_count
+      print '>>> INFO: Resetting count to ',start_count
+    else:
+      try:
+        with open('.save_state.pkl', 'rb') as handle:
+          b = pickle.load(handle)
+          if b.get('count') != None:
+             self.count = b.get('count');
+             print '>>> INFO: State Recovered, staring from index ',self.count,'...'
+             self.count = b.get('count');
+      except Exception, e:
+        print '>>> Error: no save state found...',str(e)
     while(1):#Opps No schular works implet by sleep. 
-      data = self.DATA_SOURCE[self.count]
-      ###  Do Some Normaliztion
-      data1={}
-      for k,v in data.items():
-          data1[k]=Normalize(v)
-      data1['id']=self.count
-      #pdb.set_trace()
-      body = self.BuildMailTemplate(self.TEMPLATE_NAME,data1);
-      subj = 'Puzzle #%s: %s....'%(self.count,data1['q'][:50]) # MUST BE CHNAGEED IF YOUR DATA CHNAGE
+      try:
+        data = self.DATA_SOURCE[self.count]
+        ###  Do Some Normaliztion
+        data1={}
+        for k,v in data.items():
+            data1[k]=Normalize(v)
+        data1['id']=self.count
+        #pdb.set_trace()
+        body = self.BuildMailTemplate(self.TEMPLATE_NAME,data1);
+        subj = 'Puzzle #%s: %s....'%(self.count,data1['q'][:50]) # MUST BE CHNAGEED IF YOUR DATA CHNAGE
 
-      body = body.encode('utf-8')
-      subj = subj.encode('utf-8')
-      self.SendMail(subject=subj,body=body);
+        body = body.encode('utf-8')
+        subj = subj.encode('utf-8')
+        res = self.SendMail(subject=subj,body=body)
+        if res == True:
+          #Increment count Saving the State...
+          self.count = self.count+1;
+          with open('.save_state.pkl', 'wb') as handle:
+            pickle.dump({'count': self.count}, handle)
+      except Exception ,e:
+        print '>>>ERROR: Some Issue while sending some message',e
       print '>>>INFO: Let"s Sleep for next Evnet....',sec,'Secons'
-      self.count = self.count+1;
       time.sleep(sec)
     
   def AttachTempalte(self,file):
@@ -117,9 +143,18 @@ class MailEngine:
         print '>>> ERROR: NOt able to attach data source ',e      
       
 #Sampel Test is Here.
+import sys
 m = MailEngine()
 m.AttachTempalte('Template.html')
 m.AttachDataSource('data.pkl')
-m.Schedule(sec=60*24*60)
+start_count = None
+if len(sys.argv) >1:
+ start_count = int(sys.argv[1])
+ print '>>> Starting count ',sys.argv[1]
+else: 
+  print 'Using save state or zero...'
+
+raw_input('>>> Click to start.....')
+m.Schedule(sec=60*24*60,start_count=start_count)
 #x = m.BuildMailTemplate('Template.html',{'q':'My Question','a':'Ans','link':'http://google.com'});
 #m.SendMail(body=x)
