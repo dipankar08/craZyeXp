497176f2cfbee2b7715471d5eab70b23408d5df7
Your Name <you@example.com>
2015-05-18 19:22:42 +0530

    Auto Checkin on Mon May 18 19:22:42 IST 2015

--- MiniProjects/genApps/publish.sh
+++ MiniProjects/genApps/publish.sh
@@ -1,4 +1,4 @@
 command -v ngrok >/dev/null 2>&1 || { echo "I require ngrok but it is not installed.  Let Install it" >&2; wget https://dl.ngrok.com/ngrok_2.0.17_linux_386.zip ; unzip ngrok_2.0.17_linux_386.zip ; sudo mv ./ngrok /usr/bin; sudo rm grok_2.0.17_linux_386.zip; }; 
 command -v ngrok >/dev/null 2>&1 || { echo "Verification !!! Opps it is not gyet installed" >&2;}
 echo 'Publising to internet by http://dipankar.ngrok.io/cleancode/0/ '
-ngrok http -auth="dipankar:cleancode"  -subdomain=dipankar 7777 
+ngrok http -auth="dipankar:CleanCode"  -subdomain=dipankar 7777 
