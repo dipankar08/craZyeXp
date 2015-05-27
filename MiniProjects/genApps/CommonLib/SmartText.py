efc6583da15eeac45506f7a895250b1bd5c22ecd
Your Name <you@example.com>
2015-05-27 20:55:13 +0530

    Auto Checkin on Wed May 27 20:55:13 IST 2015

---
+++ MiniProjects/genApps/CommonLib/SmartText.py
@@ -0,0 +1,32 @@
+############# This Smart Text to HTml ##################
+#
+# Standardization
+#  1. Starting with # is p1
+#  2. Staring with  * is p2
+#  3. {{{  Code }}}
+#  4. {{  input box }}
+#  5. starting with -  is ul li
+#  6. Staring with -- nested ul li
+#  7. Starting with 1. ol li
+#  8. Single line Break : new para
+#  9. Double line Break : New section
+#  10. ** bold ** inside line indicate bold
+#  11. ***  italic ***
+#  12. {{{REF_CODE}}} Replace by it's own code.
+#  13 ------------------- Line is a section
+#
+# Not supported:
+# http://stackoverflow.com/editing-help
+########################################################
+
+def smartTextToHtml(txt):
+  "Smart text"
+  
+
+
+
+
+
+
+
+
