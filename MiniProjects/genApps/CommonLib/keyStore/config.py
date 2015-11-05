dec8781598f5ead1e6eedd9adbe5d58816d7dbba
Your Name <you@example.com>
2015-11-05 19:01:36 +0530

    Auto Checkin on Thu Nov  5 19:01:36 IST 2015

--- MiniProjects/genApps/CommonLib/keyStore/config.py
+++ MiniProjects/genApps/CommonLib/keyStore/config.py
@@ -8,5 +8,7 @@ CONFIG ={}
 
 # Adding config for table<aa>
 CONFIG['test'] = {
-    'contrains':[('name','unique')]
+    'contrains':[('name','unique')], # add insit a list if someone is in random..
+    'getAutoRandom6':['share_id'],   # add if you want to be automated random value while insert.
+    'notnull':[] # list your attribute which can't be null 
 }
